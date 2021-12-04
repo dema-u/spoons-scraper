9ac593dfb7d5173c7060ce8b3a9e483fdc95e947
Dema Ushchapovskyy <dmytro.ushchapovskyy@clearmacro.com>
2021-12-04 18:42:53 +0000

    Remove unused imports

--- spoons_scraper/settings.py
+++ spoons_scraper/settings.py
@@ -1,18 +1,17 @@
-from pydantic import BaseSettings, Field, SecretStr, HttpUrl
+from pydantic import BaseSettings, Field, HttpUrl
 
 
 class Settings(BaseSettings):
-    
-    DB_HOST: str = Field('localhost')
+
+    DB_HOST: str = Field("localhost")
     DB_PORT: int = Field(5432)
-    DB_USER: str = Field('root')
-    DB_PASS: str = Field('root')
-    DB_NAME: str = Field('spoons')
-    
-    SPOONS_BASE_URL: HttpUrl = Field('https://www.jdwetherspoon.com')
-    SPOONS_LOCATIONS_PATH: str = Field('/site-map')
-    
+    DB_USER: str = Field("root")
+    DB_PASS: str = Field("root")
+    DB_NAME: str = Field("spoons")
+
+    SPOONS_BASE_URL: HttpUrl = Field("https://www.jdwetherspoon.com")
+    SPOONS_LOCATIONS_PATH: str = Field("/site-map")
+
     class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
-        
+        env_file = ".env"
+        env_file_encoding = "utf-8"
